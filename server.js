5c7dcfb8803996d7271428193fc38dc13d0918d5
HoSoo Lee <hosoo.lee@arithnea.de>
2020-11-16 22:25:41 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const mongoose = require("mongoose");
+
+require("dotenv").config({ path: "variables.env" });
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.error(err));
+
+const app = express();
+const PORT = process.env.PORT || 4444;
+
+app.listen(PORT, () => {
+  console.log(`Server Listening on PORT ${PORT}`);
+});
