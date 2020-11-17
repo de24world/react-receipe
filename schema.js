de73d94caf668271c9a4951389008f9892509184
HoSoo Lee <hosoo.lee@arithnea.de>
2020-11-17 18:23:24 +0100

    13. create first query

--- schema.js
+++ schema.js
@@ -1,6 +1,7 @@
 exports.typeDefs = `
 
     type Recipe {
+        _id: ID
         name: String!
         category: String!
         description: String!
@@ -12,6 +13,7 @@ exports.typeDefs = `
     }
 
     type User {
+        _id: ID
         username: String! @unique
         password: String!
         email: String!
@@ -21,6 +23,10 @@ exports.typeDefs = `
 
 type Query {
     getAllRecipes: [Recipe]
+}
+
+type Mutation {
+    addRecipe(name: String!, description: String!, category: String!, instructions: String!, username: String): Recipe
 }
 
 `;
