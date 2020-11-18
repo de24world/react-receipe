1341efd03f161c46c2677e1a7298b226f75d190b
HoSoo Lee <hosoo.lee@arithnea.de>
2020-11-18 22:13:55 +0100

    16. Add Client Dependencies and set up apolloclient/apolloprovider

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4444/graphql",
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
